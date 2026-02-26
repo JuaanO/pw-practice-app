5d91f7f0a6882edd6688b72d54022225e162eed7
Juan Jose <juanjose_@hotmail.es>
2026-02-26 18:56:25 +0100

    Locators available in playwright

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,9 +1,12 @@
 import test, { expect } from "@playwright/test";
+import { first } from "rxjs-compat/operator/first";
+
+
 
 test.describe('Suite of my first test', () => {
 
     test.beforeEach(async({page}) =>{
-        await page.goto('http://localhost:4200/pages/iot-dashboard')
+        await page.goto('http://localhost:4200/')
 
     })
     test('Open page form', async ({page}) => {
@@ -25,5 +28,49 @@ test.describe('Suite of my first test', () => {
         await expect(page.locator('xpath=//nb-card-body[contains(text(), "Lorem")]')).toBeVisible();
         await page.screenshot({path: 'screenshots/dialogs.png', fullPage: true});   
         await page.getByRole('button', {name: 'DISMISS DIALOG'}).click();     
-    }) 
+    })
+
+})
+
+test.describe('Types of locators of available in playwright', () => {
+
+    
+    test.beforeEach(async({page}) =>{
+        await page.goto('http://localhost:4200/')
+        await page.getByRole('link', {name: 'Forms'}).click();
+        await page.getByRole('link', {name: 'Form Layouts'}).click();
+
+    })
+
+    test('locators available: ', async ({page}) => {
+
+        //By tag name
+        await page.locator('input').nth(0).click()
+
+        //By ID
+        await page.locator('#inputEmail1').click()
+
+        //By class value
+        await page.locator('.shape-rectangle').nth(0).click()
+
+        //By attribute
+        await page.locator('[placeholder="Email"]').nth(0).click()
+
+        //By class value (full)
+        await page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]').nth(0).click()
+
+        //By combine differents selectors
+        await page.locator('input[placeholder="Email"]').nth(0).click()
+
+        //By Xpath
+        await page.locator('//*[@id="inputEmail1"]').click()
+
+        //By partial text match
+        await page.locator(':text("Using")').click()
+
+        //By exact text match
+        await page.locator(':text-is("Using the Grid")').click()
+    })
+    
+
 })
