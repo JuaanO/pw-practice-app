5ea5b4a7cb04ca3c6e41e3204c33e1f92a7f6c57
Juan Jose <juanjose_@hotmail.es>
2026-02-28 20:59:43 +0100

    Add timeouts to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 40000,
+  globalTimeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,11 +27,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    actionTimeout: 20000,
+    navigationTimeout: 20000,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on'
   },
 
   /* Configure projects for major browsers */
---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+    test.beforeEach(async({page}) =>{
+        await page.goto('http://uitestingplayground.com/ajax')
+        //await page.getByRole('button', {name: 'Button Triggering AJAX Request'}).click()
+
+    })
+
+
+    test.describe('New test suite', () => {
+
+
+        test('Test AJAX', async ({page}) => {
+
+            //maximun of time of test
+            test.slow()
+            await page.getByText('Button Triggering AJAX Request').click({timeout:200})
+
+            const successMessage = page.locator('.bg-success')
+            const text = await successMessage.textContent()
+
+
+            //Wait for default 30 seconds 
+            //await successMessage.click()
+            //await successMessage.waitFor({state: 'visible'})
+            //expect(text).toContain('Data loaded with AJAX get request.')
+
+            await expect(successMessage).toHaveText('Data loaded with AJAX get request.', {timeout:200})
+
+        })
+    })
